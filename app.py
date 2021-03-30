d25e9848b9f8fd6a296ffe242f879502184da7f8
raphael <rkala16@freeuni.edu.ge>
2021-03-31 00:07:39 +0400

    add code section to extract

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from PyQt5 import QtWidgets,QtCore, QtGui
 import sys
+import os
+from PyQt5 import QtWidgets,QtCore, QtGui
+from timeStamp_info import save_channel_info
 
 class all_together(QtWidgets.QMainWindow):
 
@@ -21,7 +23,7 @@ class all_together(QtWidgets.QMainWindow):
         
     def create_tab1(self):
         tab1_layout = QtWidgets.QVBoxLayout()
-
+        self.statusBar()
         group_box_browse = QtWidgets.QGroupBox("Open from")
         group_box_browse_layout = QtWidgets.QHBoxLayout()
         self.browse_text_box = QtWidgets.QLineEdit(self)
@@ -33,6 +35,7 @@ class all_together(QtWidgets.QMainWindow):
         group_box_browse_layout.addWidget(browse)
         group_box_browse.setLayout(group_box_browse_layout)
         group_box_browse.setFixedSize(300,70)
+        group_box_browse.setStatusTip("Choose hf5 format file to open")
 
         group_box_from_to = QtWidgets.QGroupBox("Select time range")
         group_box_from_to_layout = QtWidgets.QHBoxLayout()
@@ -50,15 +53,18 @@ class all_together(QtWidgets.QMainWindow):
         group_box_from_to_layout.addWidget(self.extract_to)
         group_box_from_to.setLayout(group_box_from_to_layout)
         group_box_from_to.setFixedSize(300,70)
+        group_box_from_to.setStatusTip("Choose particular time, leave empty for full time")
 
         group_box_channel_stream = QtWidgets.QGroupBox("Select id")
         group_box_channel_stream_layout = QtWidgets.QHBoxLayout()
         self.channel_id = QtWidgets.QLineEdit(self)
         self.channel_id.setFixedWidth(70)
+        self.channel_id.setStatusTip("Choose particular channel, leave empty for all Channels")
         channel_id_label = QtWidgets.QLabel(self)
         channel_id_label.setText("Channel_id:")
         self.stream_id = QtWidgets.QLineEdit(self)
         self.stream_id.setFixedWidth(70)
+        self.stream_id.setStatusTip("Choose particular stream, leave empty for all Streams")
         stream_id_label = QtWidgets.QLabel(self)
         stream_id_label.setText("Stream_id:")
         group_box_channel_stream_layout.addWidget(channel_id_label)
@@ -74,12 +80,12 @@ class all_together(QtWidgets.QMainWindow):
         self.extract_text_box.setDisabled(True)
         extract = QtWidgets.QPushButton(self)
         extract.setText("Extract")
-        # extract.clicked.connect(lambda x: self.error_popup("aq gamova is teqsti rasac metodi daabrunebs. magalitad channel id is incorrect"))
         extract.clicked.connect(self.extract_file)
         group_box_extract_layout.addWidget(self.extract_text_box)
         group_box_extract_layout.addWidget(extract)
         group_box_extract.setLayout(group_box_extract_layout)
         group_box_extract.setFixedSize(300,70)
+        group_box_extract.setStatusTip("Choose path to save csv file")
 
         tab1_layout.addWidget(group_box_browse)
         tab1_layout.addWidget(group_box_from_to)
@@ -96,10 +102,28 @@ class all_together(QtWidgets.QMainWindow):
         self.browse_text_box.setText(fileName)
     
     def extract_file(self):
-        print(f'browse: {self.browse_text_box.text()}')
-        print(f'from: {self.extract_from.text()} to: {self.extract_to.text()}')
-        print(f'channel id: {self.channel_id.text()} stream id: {self.stream_id.text()} ')
-        print(f'extract: {self.extract_text_box.text()}')
+        analog_stream_path = self.browse_text_box.text()
+        stream_id = self._check_value(self.stream_id.text(),None)
+        channel_id = self._check_value(self.channel_id.text(),None)
+        from_in_s = self._check_value(self.extract_from.text(),0)
+        to_in_s = self._check_value(self.extract_to.text(),None)
+        if -1 in (stream_id,channel_id,from_in_s,to_in_s):
+            self.error_popup("Please enter correct values")
+            return
+        name, _ = QtWidgets.QFileDialog.getSaveFileName(self,'Save File', options=QtWidgets.QFileDialog.DontUseNativeDialog)
+        self.extract_text_box.setText(name)
+        file_save_path = self.extract_text_box.text()
+        save_error, value = save_channel_info(analog_stream_path, file_save_path, channel_id=channel_id, from_in_s=from_in_s, to_in_s=to_in_s)
+        if save_error:
+            self.error_popup(value)
+        
+    def _check_value(self,value,res):
+        if value == "":
+            return res
+        try:
+            return float(value)
+        except ValueError:
+            return -1
 
     def error_popup(self,txt):
         QtWidgets.QMessageBox.critical(self,"Unable to Extract",txt)
