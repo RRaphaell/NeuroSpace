1a584304a485e1a1f4cf6ddd6bcb6609867d1f36
tatia <tatiatsmindashvili@pulsar.ai>
2021-07-17 19:44:14 +0400

    add reduce functional

--- timeStamp_info.py
+++ timeStamp_info.py
@@ -38,13 +38,14 @@ def _get_signal_cutouts(signal, fs, spikes_idx, pre, post):
     cutouts = []
     pre_idx = int(pre * fs)
     post_idx = int(post * fs)
-    for index in spikes_idx:
-        if index-pre_idx >= 0 and index+post_idx <= signal.shape[0]:
-            cutout = signal[(index-pre_idx):(index+post_idx)]
-            cutouts.append(cutout)
+    if pre_idx > 0 and post_idx > 0:
+        for index in spikes_idx:
+            if index-pre_idx >= 0 and index+post_idx <= signal.shape[0]:
+                cutout = signal[(index-pre_idx):(index+post_idx)]
+                cutouts.append(cutout)
 
-    if (len(cutouts)>0) :
-        return np.stack(cutouts)
+        if (len(cutouts)>0) :
+            return np.stack(cutouts)
     return cutouts
 
 def _plot_each_spike(ax, cutouts, fs, pre, post, n=100, color='k'):
@@ -301,8 +302,7 @@ def _plot_all_spikes_together(signal_in_uV, channel_id, channel_id_converted, fs
     axes = canvas.figure.get_axes()
     ax = axes[suplot_num]
     ax.clear()
-    print(suplot_num)
-    if (len(spks) < 2):
+    if (len(cutouts) < 2):
         ax.set_title('No Spike')
         canvas.figure.tight_layout()
         return []
@@ -461,49 +461,62 @@ def _calculate_average_signal(electrode_stream, channel_id, from_in_s, to_in_s):
     return signal_in_uV, channel_id_converted, time_in_sec
 
 def _get_spikes_dataframe_to_extract(electrode_stream, channel_ids, from_in_s, signal_if_avg, to_in_s, threshold_from, threshold_to, high_pass, low_pass, dead_time, 
-                                bin_width, max_start, max_end, min_between, min_duration, min_number_spike, stimulus, pre, post, n_components):
+                                bin_width, max_start, max_end, min_between, min_duration, min_number_spike, stimulus, pre, post, n_components, reduce_num):
                                 
     spikes_df = pd.DataFrame()
     bins_df = pd.DataFrame()
-
     _, time_in_sec = _get_signal_time(electrode_stream, channel_ids[0], from_in_s, to_in_s) 
     fs = int(electrode_stream.channel_infos[0].sampling_frequency.magnitude)
+    fs_reduced = fs
     from_idx, to_idx = _check_time_range(electrode_stream, fs, from_in_s, to_in_s)
+    len_of_spike_df = to_idx - from_idx +1 
+    
     if to_in_s is None:
         to_idx-=1
-    spikes_df["time"] = time_in_sec
 
     if bin_width:
         bin_ranges = [bin_width*i for i in list(range(int(np.ceil((to_idx-from_idx+1)/fs/bin_width))))]
         bins_df["bin_ranges"] = np.array(bin_ranges)+ from_in_s
+
+    if reduce_num and reduce_num > 0:
+        len_of_spike_df /= reduce_num
+        len_of_spike_df = int(len_of_spike_df) +1
 
     for channel_id in channel_ids:
         if len(signal_if_avg)>0:
             channel_label = "avg"
             signal_if_avg /= 1000000
             signal_if_avg = _filter_base_freqeuncy(signal_if_avg, fs, high_pass, low_pass)
+            if reduce_num and reduce_num > 0:
+                signal_if_avg, fs_reduced = _reduce_signal(signal_if_avg, fs, int(reduce_num))
+                time_in_sec = np.arange(0, len(signal_if_avg))/fs_reduced
+            spikes_df["time"] = time_in_sec
             spikes_df["signal_avg"] = signal_if_avg  # This part is for average signal, to save the voltage of average signal
             threshold_from, threshold_to = _get_proper_threshold(signal_if_avg, threshold_from, threshold_to, True)
-            spks = _get_spike_info(electrode_stream, 0, signal_if_avg, threshold_from, threshold_to, dead_time)
+            spks = _get_spike_info(signal_if_avg, fs_reduced, threshold_from, threshold_to, dead_time)
             spks = np.array(list(filter(lambda x: signal_if_avg[x]>=threshold_to, spks)))
-            cutouts = _get_signal_cutouts(signal_if_avg, fs, spks, pre, post)
+            cutouts = _get_signal_cutouts(signal_if_avg, fs_reduced, spks, pre, post)
         else:
             channel_label = electrode_stream.channel_infos.get(channel_id).info['Label']            
             signal = electrode_stream.get_channel_in_range(channel_id, from_idx, to_idx)[0]
             signal = _filter_base_freqeuncy(signal, fs, high_pass, low_pass)
-            threshold_from, threshold_to = _get_proper_threshold(signal, threshold_from, threshold_to, True)
-            spks = _get_spike_info(electrode_stream, channel_id, signal, threshold_from, threshold_to, dead_time)
+            if reduce_num and reduce_num > 0:
+                signal, fs_reduced = _reduce_signal(signal, fs, int(reduce_num))
+                time_in_sec = np.arange(0, len(signal))/fs_reduced
+            spikes_df["time"] = time_in_sec
+            threshold_from, threshold_to = _get_proper_threshold(signal, threshold_from, threshold_to, True)       
+            spks = _get_spike_info(signal, fs_reduced, threshold_from, threshold_to, dead_time)
             spikes_df["signal_"+str(channel_label)] = signal
             spks = np.array(list(filter(lambda x: signal[x]>=threshold_to, spks)))
-            cutouts = _get_signal_cutouts(signal, fs, spks, pre, post)
+            cutouts = _get_signal_cutouts(signal, fs_reduced, spks, pre, post)
 
         if len(spks)<1:
-            spikes_df["spikes"+str(channel_label)] = np.zeros(to_idx-from_idx+1)
-            spikes_df["bursts"+str(channel_label)] = np.zeros(to_idx-from_idx+1)
+            spikes_df["spikes"+str(channel_label)] = np.zeros(int(len_of_spike_df))
+            spikes_df["bursts"+str(channel_label)] = np.zeros(int(len_of_spike_df))
             continue 
 
-        to_be_spikes = np.zeros(to_idx-from_idx+1)
-        if len(spks) >=2:
+        to_be_spikes = np.zeros(int(len_of_spike_df))
+        if len(cutouts) >=2:
             labels = _get_pca_labels(cutouts, n_components)
             labels = labels + 1
         else:
@@ -519,22 +532,22 @@ def _get_spikes_dataframe_to_extract(electrode_stream, channel_ids, from_in_s, s
         if (not (None in [max_start, max_end, min_between, min_duration, min_number_spike])):
             bursts_starts, _ = _get_burst(spikes_in_second, max_start, max_end, min_between, min_duration, min_number_spike)
             bursts_starts = (np.array(bursts_starts)*fs).astype(int)
-            to_be_bursts = np.zeros(to_idx-from_idx+1)
+            to_be_bursts = np.zeros(int(len_of_spike_df))
             to_be_bursts[bursts_starts] = 1
             spikes_df["bursts"+str(channel_label)] = to_be_bursts
         else:
-            spikes_df["bursts"+str(channel_label)] = np.zeros(to_idx-from_idx+1)
+            spikes_df["bursts"+str(channel_label)] = np.zeros(int(len_of_spike_df))
         
     if len(stimulus) > 0 :
         stimulus = (np.array(stimulus)*fs).astype(int)
         stimulus_in_range = stimulus[(stimulus >= from_idx) & (stimulus <= to_idx)]
         stimulus_in_range-=from_idx
-        to_be_stimulus = np.zeros(to_idx-from_idx+1)
+        to_be_stimulus = np.zeros(int(len_of_spike_df))
         to_be_stimulus[stimulus_in_range] = 1
         spikes_df["stimulus"] = to_be_stimulus
     else:
-        spikes_df["stimulus"] = np.zeros(to_idx-from_idx+1)
-
+        spikes_df["stimulus"] = np.zeros(int(len_of_spike_df))
+    
     return spikes_df, bins_df
 
 def _reduce_signal(signal, fs, reduce_num):
@@ -560,7 +573,7 @@ def plot_tab1(electrode_stream, channel_id, from_in_s, to_in_s, canvas, high_pas
     signal_in_uV = _filter_base_freqeuncy(signal_in_uV, fs, high_pass, low_pass)
     _plot_signal(signal_in_uV, "Original Signal", time_in_sec, canvas, 0)
 
-    if int(check_boxes[1]):
+    if check_boxes[1] and int(check_boxes[1]) > 0:
         signal_in_uV, fs = _reduce_signal(signal_in_uV, fs, int(check_boxes[1]))
         time_in_sec = np.arange(0, len(signal_in_uV))/fs
         _plot_signal(signal_in_uV, "After Reducing", time_in_sec, canvas, 1)
@@ -570,12 +583,16 @@ def plot_tab1(electrode_stream, channel_id, from_in_s, to_in_s, canvas, high_pas
     return 0,""
 
 def plot_tab2(electrode_stream, channel_id, from_in_s, to_in_s, high_pass, low_pass, threshold_from, threshold_to, dead_time, check_boxes, pre, post, canvas,
-                comp_number, spike_number, stimulus, max_start, max_end, min_between, min_duration, min_number_spike, bin_width):
+                comp_number, spike_number, stimulus, max_start, max_end, min_between, min_duration, min_number_spike, bin_width, reduce_num):
 
     signal_in_uV , channel_id_converted, time_in_sec = _calculate_average_signal(electrode_stream, channel_id, from_in_s, to_in_s)
     fs = int(electrode_stream.channel_infos[0].sampling_frequency.magnitude)
     signal_in_uV = _filter_base_freqeuncy(signal_in_uV, fs, high_pass, low_pass)  
     from_idx, to_idx = _check_time_range(electrode_stream, fs, from_in_s, to_in_s)  
+
+    if reduce_num and reduce_num > 0:
+        signal_in_uV, fs = _reduce_signal(signal_in_uV, fs, int(reduce_num))
+        time_in_sec = np.arange(0, len(signal_in_uV))/fs
 
     if check_boxes[0].isChecked():
         labels = _plot_all_spikes_together(signal_in_uV, channel_id, channel_id_converted, fs, comp_number, pre, post, dead_time, spike_number,
@@ -598,13 +615,16 @@ def plot_tab2(electrode_stream, channel_id, from_in_s, to_in_s, high_pass, low_p
     
     return 0, ""
 
-def plot_tab3(electrode_stream, channel_id, from_in_s, to_in_s, high_pass, low_pass, threshold_from, threshold_to, dead_time, check_boxes, pre, post, canvas):
-
+def plot_tab3(electrode_stream, channel_id, from_in_s, to_in_s, high_pass, low_pass, threshold_from, threshold_to, dead_time, check_boxes, pre, post, canvas, reduce_num):
 
     signal_in_uV , channel_id_converted, time_in_sec = _calculate_average_signal(electrode_stream, [channel_id], from_in_s, to_in_s)
     fs = int(electrode_stream.channel_infos[0].sampling_frequency.magnitude)
     signal_in_uV = _filter_base_freqeuncy(signal_in_uV, fs, high_pass, low_pass)  
-    from_idx, to_idx = _check_time_range(electrode_stream, fs, from_in_s, to_in_s)  
+    from_idx, to_idx = _check_time_range(electrode_stream, fs, from_in_s, to_in_s) 
+
+    if reduce_num and reduce_num > 0:
+        signal_in_uV, fs = _reduce_signal(signal_in_uV, fs, int(reduce_num))
+        time_in_sec = np.arange(0, len(signal_in_uV))/fs
 
     if check_boxes[0].isChecked():
         _plot_stimulus_average(signal_in_uV, channel_id, channel_id_converted, fs, from_in_s, to_in_s, from_idx, to_idx, dead_time, threshold_from, pre, post, canvas, 0)
@@ -625,29 +645,39 @@ def plot_tab3(electrode_stream, channel_id, from_in_s, to_in_s, high_pass, low_p
     return 0, stimulus
 
 
-def extract_waveform(analog_stream, file_save_path, channel_id, from_in_s, to_in_s, high_pass, low_pass, stream_id=0):   
+def extract_waveform(analog_stream, file_save_path, channel_id, from_in_s, to_in_s, high_pass, low_pass, reduce_num):   
     _channel_info = analog_stream.channel_infos[0]
     sampling_frequency = _channel_info.sampling_frequency.magnitude
     from_idx ,to_idx = _check_time_range(analog_stream,sampling_frequency,from_in_s,to_in_s)  # get start and end index
     time = analog_stream.get_channel_sample_timestamps(0, from_idx, to_idx)  # get the timestamps for each sample 
     scale_factor_for_second = Q_(1,time[1]).to(ureg.s).magnitude  # scale time to seconds
     time_in_sec = time[0] * scale_factor_for_second
+    fs = int(analog_stream.channel_infos[0].sampling_frequency.magnitude)
+    fs_reduced = fs
     df=pd.DataFrame()
-    df["time_in_sec"] = time_in_sec
-
     if (channel_id==None):
         for channel in analog_stream.channel_infos:     
-            channel_label = analog_stream.channel_infos.get(channel).info['Label'] 
-            df["signal for "+str(channel_label)] = (_get_specific_channel_signal(analog_stream,channel,from_in_s,to_in_s,high_pass,low_pass))/1000000
+            channel_label = analog_stream.channel_infos.get(channel).info['Label']
+            signal = (_get_specific_channel_signal(analog_stream,channel,from_in_s,to_in_s,high_pass,low_pass))/1000000
+            if reduce_num and reduce_num > 0:
+                signal, fs_reduced = _reduce_signal(signal, fs, int(reduce_num))
+            df["signal for "+str(channel_label)] = signal
+        time_in_sec = np.arange(0, len(signal))/fs_reduced
+        df["time_in_sec"] = time_in_sec
     else:
         channel_label = channel_id
         channel_id = _get_channel_ID(analog_stream, channel_id)
-        df["signal for "+str(channel_label)] = (_get_specific_channel_signal(analog_stream,channel_id,from_in_s,to_in_s,high_pass,low_pass))/1000000
+        signal = (_get_specific_channel_signal(analog_stream,channel_id,from_in_s,to_in_s,high_pass,low_pass))/1000000
+        if reduce_num and reduce_num > 0:
+            signal, fs = _reduce_signal(signal, fs, int(reduce_num))
+            time_in_sec = np.arange(0, len(signal))/fs
+        df["signal for "+str(channel_label)] = signal
+        df["time_in_sec"] = time_in_sec
     
     df.to_csv(file_save_path+".csv", index=False)
     gc.collect()
 
-def extract_stimulus(analog_stream, file_save_path, channel_id, from_in_s, to_in_s, stimulus_threshold, dead_time, pre, post, high_pass, low_pass):
+def extract_stimulus(analog_stream, file_save_path, channel_id, from_in_s, to_in_s, stimulus_threshold, dead_time, pre, post, high_pass, low_pass, reduce_num):
     _channel_info = analog_stream.channel_infos[0]
     fs = _channel_info.sampling_frequency.magnitude
     from_idx, to_idx = _check_time_range(analog_stream, fs, from_in_s, to_in_s)
@@ -665,22 +695,22 @@ def extract_stimulus(analog_stream, file_save_path, channel_id, from_in_s, to_in
     gc.collect()
 
 def extract_spike(file_path, analog_stream, file_save_path, channel_id, from_in_s, to_in_s, threshold_from, threshold_to, high_pass, low_pass, dead_time, bin_width,
-                max_start, max_end, min_between, min_duration, min_number_spike, stimulus, pre, post, n_cpmponents):
+                max_start, max_end, min_between, min_duration, min_number_spike, stimulus, pre, post, n_cpmponents, reduce_num):
 
     if len(channel_id) == 1 :
         if channel_id[0] == "all":
             channel_ids = [key for key, value in analog_stream.channel_infos.items()]
             spikes_df,bins_df = _get_spikes_dataframe_to_extract(analog_stream, channel_ids, from_in_s, [], to_in_s, threshold_from, threshold_to, high_pass, low_pass, dead_time,
-                                                                    bin_width, max_start, max_end, min_between, min_duration, min_number_spike, stimulus, pre, post, n_cpmponents)
+                                                                    bin_width, max_start, max_end, min_between, min_duration, min_number_spike, stimulus, pre, post, n_cpmponents, reduce_num)
         else:
             channel_id_converted = [_get_channel_ID(analog_stream, int(channel_id[0]))]
             spikes_df,bins_df = _get_spikes_dataframe_to_extract(analog_stream, channel_id_converted, from_in_s, [], to_in_s, threshold_from, threshold_to, high_pass, low_pass, dead_time,
-                                                                        bin_width, max_start, max_end, min_between, min_duration, min_number_spike, stimulus, pre, post, n_cpmponents)
+                                                                        bin_width, max_start, max_end, min_between, min_duration, min_number_spike, stimulus, pre, post, n_cpmponents, reduce_num)
     else:
         signal_if_avg,_,_ =  _calculate_average_signal(analog_stream, channel_id, from_in_s, to_in_s)
         channel_ids = [0]
         spikes_df, bins_df = _get_spikes_dataframe_to_extract(analog_stream, channel_ids, from_in_s, signal_if_avg, to_in_s, threshold_from, threshold_to, high_pass, low_pass, dead_time,
-                                                                    bin_width, max_start, max_end, min_between, min_duration, min_number_spike, stimulus, pre, post, n_cpmponents)
+                                                                    bin_width, max_start, max_end, min_between, min_duration, min_number_spike, stimulus, pre, post, n_cpmponents, reduce_num)
     
     # save part of DataFrames
     if spikes_df.size > 0 :
