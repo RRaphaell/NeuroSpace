2523d4fcb5d3ffd975deff9f65ebd12f68db4d82
RRaphaell <raffo.kalandadze@gmail.com>
2021-09-30 00:36:20 +0400

    fix bugs whe there was one spike

--- Modules/Bin.py
+++ Modules/Bin.py
@@ -5,7 +5,6 @@ from Modules.Spikes import Spikes
 class Bin(Spikes):
     def __init__(self, bin_width, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        print(bin_width)
         self._plot_func = None
         self.bin_width = bin_width
         
